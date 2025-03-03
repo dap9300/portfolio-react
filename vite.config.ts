c851269d653375d0d81349d02d56bdebda4550ea
dap9300 <161590867+dap9300@users.noreply.github.com>
2025-03-03 13:11:17 +0000

    repush dopo modifica 4 package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
-// vite.config.ts 
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path, { dirname } from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
+  plugins: [react(), themePlugin()],
   resolve: {
     alias: {
- 
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist", // Vercel si aspetta la cartella dist
     emptyOutDir: true,
   },
   server: {
@@ -30,9 +23,4 @@ export default defineConfig({
       overlay: false
     }
   },
-  preview: {
-    port: 5000,
-    strictPort: true,
-    host: true,
-  }
 });
